e9f1b5afb27e3d5b457b14f5ffb5aca1b06a84eb
mac-pro(tutor borrowed P.terk) <tutorship57@gmail.com>
2024-12-10 22:13:36 +0700

    fix "add fetch.js"

--- product-list-with-cart(Mentor2)/fetch.js
+++ product-list-with-cart(Mentor2)/fetch.js
@@ -7,12 +7,36 @@ async function downloadData() {
         const data = await response.json()
         console.log("fetch success *-*");
         console.log(data);
-        data.forEach(item => {
-            const listItem = document.createElement('li')
-            listItem.textContent = `image: ${item.image.desktop}, Name: ${item.name}, Prize: ${item.price}` 
-            dataList.appendChild(listItem);
-            console.log(item);
-         });
+        for (let index = 0; index < data.length; index++) {
+            const element = data[index];
+            const DivTag = document.createElement('div');
+            let priceCasting = parseFloat(element.price).toFixed(2);
+    
+            
+            DivTag.classList.add('menu');
+            DivTag.setAttribute('name',index);
+            DivTag.innerHTML = `<div class="pictureAndCart">
+            <img src="${element.image.desktop}" alt="">
+            <div class="add-tocart"><img class="icon" src="./assets/images/icon-add-to-cart.svg"> Add to Cart</div>
+            <div class="have-Incart" id="a">
+              <div class="iconIncart decreaseIcon"><svg xmlns="http://www.w3.org/2000/svg" width="10" height="2"
+                  fill="none" viewBox="0 0 10 2">
+                  <path fill="#fff" d="M0 .375h10v1.25H0V.375Z" />
+                </svg></div>
+              <div class="quantity"></div>
+              <div class="iconIncart increaseIcon"><svg xmlns="http://www.w3.org/2000/svg" width="10" height="10"
+                  fill="none" viewBox="0 0 10 10">
+                  <path fill="#fff" d="M10 4.375H5.625V0h-1.25v4.375H0v1.25h4.375V10h1.25V5.625H10v-1.25Z" />
+                </svg></div>
+            </div>
+          </div>
+          <div class="category">${element.category}</div>
+          <div class="menuName">${element.name}</div>
+          <div class="price">$${priceCasting}</div>
+            `
+            const menuShowmenu = document.querySelector('.menu-showMenu')
+            menuShowmenu.appendChild(DivTag);
+        }
         return data 
     } catch (error) {   
         console.log("this is error : "+error );
